dd3ac47aad5a5a9eac7114b27113542e235f804d
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-01 16:01:56 +0600

    added sign in with google functionality

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,46 @@
 import { useContext } from 'react';
 import {toast,ToastContainer} from 'react-toastify'
 import { AuthContext } from '../Providers/AuthProvider';
+import { FcGoogle } from "react-icons/fc";
+import { GrFacebookOption } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
 
 const Login = () => {
-    const {signin,setLoading} = useContext(AuthContext)
+    const {signin,setLoading,googleAuth,facebookAuth,githubAuth} = useContext(AuthContext)
+
+    const googleSignin = ()=>{
+        googleAuth()
+        .then(()=>{
+          setLoading(false)
+          toast.success('You have logged in successfully')
+        })
+        .catch(()=>{
+          setLoading(false)
+            toast.error('Something went wrong')
+        })
+    }
+    const githubSignin = ()=>{
+        googleAuth()
+        .then(()=>{
+          setLoading(false)
+          toast.success('You have logged in successfully')
+        })
+        .catch(()=>{
+          setLoading(false)
+            toast.error('Something went wrong')
+        })
+    }
+    const facebookSignin = ()=>{
+        googleAuth()
+        .then(()=>{
+          setLoading(false)
+          toast.success('You have logged in successfully')
+        })
+        .catch(()=>{
+          setLoading(false)
+            toast.error('Something went wrong')
+        })
+    }
 
     const submit = (e)=>{
       e.preventDefault()
@@ -36,6 +73,11 @@ const Login = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className='flex justify-center items-center gap-4'>
+      <FcGoogle className='text-2xl' role='button' onClick={googleSignin}/>
+      <FaGithub className='text-2xl' role='button' onClick={githubSignin}/>
+        <GrFacebookOption className='text-2xl text-[#0866FF] bg-white' role='button' onClick={facebookSignin}/>
+      </div>
       <form className="card-body" onSubmit={submit}>
         <div className="form-control">
           <label className="label">
