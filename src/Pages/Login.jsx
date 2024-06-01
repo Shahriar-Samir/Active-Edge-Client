a96ad81415b2ee2430b537f26133586ce687b399
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-01 15:54:23 +0600

    added login page with login funcitionality

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import { useContext } from 'react';
+import {toast,ToastContainer} from 'react-toastify'
+import { AuthContext } from '../Providers/AuthProvider';
 
 const Login = () => {
+    const {signin,setLoading} = useContext(AuthContext)
+
+    const submit = (e)=>{
+      e.preventDefault()
+      const form = e.target
+      const email= form.email.value
+      const password = form.password.value
+          signin(email,password)
+          .then(res=>{
+            setLoading(false)
+            toast.success('You have logged in successfully')
+          })
+          .catch(err=>{
+            setLoading(false)
+            if(err.message === 'Firebase: Error (auth/invalid-credential).'){
+              toast.error('Invalid email or password')
+            }
+            else{
+              toast.error('Something went wrong')
+            }
+          })
+    }
+
     return (
         <div>
+          <ToastContainer/>
             <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
@@ -10,21 +36,18 @@ const Login = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form className="card-body" onSubmit={submit}>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">User Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
