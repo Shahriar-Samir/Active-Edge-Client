516c1195e3ce11256e30c2b21592b06cf2e6fb52
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-03 17:07:20 +0600

    added submit post and save post to database functionality in add new post or forum page

--- src/Pages/Dashboard/Pages/AddNewForum.jsx
+++ src/Pages/Dashboard/Pages/AddNewForum.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import {toast, ToastContainer} from 'react-toastify'
 
 const AddNewForum = () => {
+      const axiosSecure = useAxiosSecure()
+      const presentTime = new Date();
+
+  const postSubmit = (e)=>{
+        e.preventDefault()
+        const form = e.target 
+        const title = form.title.value
+        const texts = form.texts.value
+       axiosSecure.post('/addForumPost',{title,texts, date:presentTime.toLocaleString()})
+       .then(res=>{
+            toast.success('Post submitted successfully!')
+       })
+       .catch(()=>{
+            toast.error('Something went wrong')
+       })
+  }
+
     return (
         <div>
+          <ToastContainer/>
            <h1 className='text-xl text-center font-bold mt-5'>Add New Forum</h1>
-           <form className="card-body w-11/12 max-w-[600px] mx-auto">
+           <form className="card-body w-11/12 max-w-[600px] mx-auto" onSubmit={postSubmit}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Title</span>
           </label>
-          <input type="text" placeholder="Class Name" className="input input-bordered" required />
+          <input type="text" placeholder="Title" name="title" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Post</span>
           </label>
-          <textarea placeholder="Image" className="input input-bordered min-h-[30vh] h-[35vh] max-h-[40vh] p-4" required ></textarea>
+          <textarea placeholder="write something" name="texts" className="input input-bordered min-h-[30vh] h-[35vh] max-h-[40vh] p-4" required ></textarea>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-primary">Post</button>
         </div>
       </form>
         </div>
