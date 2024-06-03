c0097f4727ebce64cf49b2f418e01aa07b321fce
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-03 18:13:56 +0600

    fecthed all forum posts from database and organized them on community or forum page

--- src/Pages/Forum.jsx
+++ src/Pages/Forum.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query'
+import useAxiosSecure from '../Hooks/useAxiosSecure';
+import { formatDistanceToNow } from 'date-fns';
 
 const Forum = () => {
+    const axiosSecure = useAxiosSecure()
+    const {data} = useQuery({
+        queryKey:["posts"],
+        queryFn: ()=>
+            axiosSecure.get('/forumPosts')
+            .then(res=>{
+                return res.data
+            })
+    })
+
     return (
         <div className='w-11/12 mx-auto max-w-[1200px]'>
 <div className='flex flex-col max-w-[700px] gap-5 mx-auto'>
-        {[1,2,3,4,5,6].map(item=>{
-            return <Post/>
+        {data?.map(item=>{
+            return <Post post={item}/>
         })}
         </div>
         </div>
@@ -14,30 +27,26 @@ const Forum = () => {
 
 export default Forum;
 
-const Post = ()=>{
+const Post = ({post})=>{
+    const {texts,title,date} = post
+    const timeAgo = formatDistanceToNow(new Date(date), { addSuffix: true });
+
     return(
         <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
+            
         <div className="flex justify-between items-center mb-5 text-gray-500">
-            <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                Tutorial
-            </span>
-            <span className="text-sm">14 days ago</span>
-        </div>
-        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How to quickly deploy a static website</a></h2>
-        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
         <div className="flex justify-between items-center">
             <div className="flex items-center space-x-4">
                 <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                <span className="font-medium dark:text-white">
+                <span className="font-medium ">
                     Jese Leos
                 </span>
             </div>
-            <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                Read more
-                <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </a>
         </div>
+            <span className="text-sm">{timeAgo}</span>
+        </div>
+        <h2 className="mb-2 text-lg font-bold "><a href="#">{title}</a></h2>
+        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{texts}</p>
     </article>
     )
 }
