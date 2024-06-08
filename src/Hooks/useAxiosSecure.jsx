4440905573efbd6ac6f1809302d0d6efa0ed1d4b
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-09 02:50:51 +0600

    added axios interceptor that logouts when the server route responses with status 401 and 403

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
@@ -8,6 +9,8 @@ const axiosSecure = axios.create({
 })
 const useAxiosSecure = () => {
     const navigate = useNavigate()
+    const {signout} = useAuth()
+
     axiosSecure.interceptors.request.use(config=>{
         const token = localStorage.getItem('access-token')
         config.headers.authorization = `bearer ${token}`
@@ -18,9 +21,10 @@ const useAxiosSecure = () => {
 
     axiosSecure.interceptors.response.use(response=>{
         return response
-    }, error=>{
+    }, async error=>{
         const status = error.response.status
         if(status === 401 || status === 403){
+            await signout()
             navigate('/login')
         }
         return Promise.reject(error)
