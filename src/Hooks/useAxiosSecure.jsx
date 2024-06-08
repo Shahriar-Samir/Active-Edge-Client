6bf34984c354f12b21aec64c441efbc4d8624beb
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-09 00:07:33 +0600

    added axios interceptor in axiosSecure hook

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -6,6 +6,13 @@ const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(config=>{
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `bearer ${token}`
+        return config
+    }, (error=>{
+        return Promise.reject(error)
+    }))
     return axiosSecure
 };
 
