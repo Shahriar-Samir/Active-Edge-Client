c5786a7928648edd39b1a1e703c23de3b1a456d5
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-09 21:29:11 +0600

    designed banner section of the home page

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -9,11 +9,11 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div className='h-[80vh] flex items-center justify-center gap-5 w-11/12 mx-auto max-w-[1000px]'>
-            <div className='flex flex-col justify-center items-center w-1/2 h-full'>
-                <h1>Elevate Your Fitness Journey</h1>
-                <p>Join our cutting-edge platform to track progress, set goals, and connect with a vibrant fitness community. Start transforming your lifestyle today!</p>
-                <Link><button>Classes</button></Link>
+        <div className='h-[100vh] flex items-center justify-center gap-5 w-11/12 mx-auto max-w-[1000px]'>
+            <div className='flex flex-col justify-center items-start w-1/2 gap-4 h-full'>
+                <h1 className='text-7xl font-bold'>Elevate Your Fitness Journey</h1>
+                <p className='text-start text-lg'>Join our cutting-edge platform to track progress, set goals, and connect with a vibrant fitness community. Start transforming your lifestyle today!</p>
+                <Link to='/allClasses'><button className='btn bg-bgCommon text-white font-bold '>Classes</button></Link>
             </div>
               <Swiper
         spaceBetween={30}
