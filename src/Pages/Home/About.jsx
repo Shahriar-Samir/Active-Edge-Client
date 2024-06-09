8ef360f01122b7faec2be101f89d565b8eac765d
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-09 22:28:52 +0600

    added about us secion

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -1,37 +1,28 @@
 import React from 'react';
+import Heading from '../../Components/Heading';
 
 const About = () => {
     return (
-       <div className='flex justify-between h-full'>
+       <div className='mt-28'>
+        <h1 className='text-center text-5xl font-bold'>Who Are We?</h1>
+<div className='flex justify-between w-11/12 mx-auto max-w-[1200px] items-center gap-10 h-full mt-10'>
          <div className='w-1/2'>
-            <h1>Who We Are?</h1>
-            Introduction Paragraph:
+           <p className='text-xl'>
+           Introduction Paragraph:
 At the forefront of revolutionizing the fitness industry, our company is dedicated to providing innovative solutions that empower individuals to lead healthier, more active lifestyles. Combining cutting-edge technology with a passion for fitness, we create a community-driven platform that inspires and supports our users.
 <br></br>
 Mission Statement:
 Our mission is to transform fitness and wellness through innovative technology and a supportive community.
 <br></br>
 Company History:
-Founded in 2015, we’ve grown from a small startup to an industry leader, launching our first fitness tracker app in 2016 and reaching 1 million users by 2018.
+Founded in 2015, we’ve grown from a small startup to an industry leader, launching our first fitness tracker app in 2024.
+           </p>
 <br></br>
-Team Introduction:
-
-John Doe, CEO & Founder
-
-John’s vision and dedication drive our mission to innovate in fitness technology.
-Jane Smith, CTO
-
-Jane ensures our platform operates seamlessly and efficiently.
-Core Values:
-
-Innovation: Embracing the latest tech for cutting-edge solutions.
-Community: Fostering a supportive, motivating environment.
-Health: Promoting overall wellness.
-Support: Helping users achieve their fitness goals.
         </div>
         <div className='w-1/2'>
-            <img src=''/>
+            <img src='/photos/about.jpg'/>
         </div>
+       </div>
        </div>
     );
 };
