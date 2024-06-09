01e7fd9af020c61bac2fcd8f0935d632642b0e35
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-09 23:51:45 +0600

    designed latest posts section in home page

--- src/Pages/Home/LatestPosts.jsx
+++ src/Pages/Home/LatestPosts.jsx
@@ -1,32 +1,43 @@
 import { Link } from "react-router-dom";
 import Heading from "../../Components/Heading";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../Components/Loading";
 
 
 const LatestPosts = () => {
+    const axiosPublic = useAxiosPublic()
+    const {data:posts,isFetching} = useQuery({
+        queryKey:["posts"],
+        initialData: [],
+        queryFn: ()=>
+            axiosPublic.get('/forumPosts')
+            .then(res=>{
+                console.log(res.data)
+                return res.data
+            })
+    })
+    if(isFetching){
+        return <Loading/>
+    }
     return (
-        <div>
-            <Heading title={'Latest Posts'} details={'This is all posts'}/>
-            <div className="grid grid-cols-3 grid-rows-3 h-[70vh]">
-                {[1,2,3,4,5].map((item,index)=>{
-                   if(index===0){
-                    return <div key={index} className="row-span-3 col-span-1 p-4" >
-                            <div className="flex items-center gap-4">
-                            <img src="" className="w-[50px] h-[50px]"/>
-                            <h1>Masud Rana</h1>
-                            </div>
-                            <h1 className="text-3xl font-semibold">5 Tips to Kickstart Your Fitness Journey</h1>
-                            <p>Discover essential tips and strategies to begin your fitness journey and stay motivated. These practical steps will help you set achievable goals and maintain a healthy routine.</p>
-                            <Link><button>Read More</button></Link>
-                    </div>
-                   }
+        <div className="w-11/12 mx-auto max-w-[1200px] mt-28">
+            <Heading title={'Latest Posts'} details={'Six latest forum posts'}/>
+            <div className="grid grid-cols-2 gap-7 mt-10">
+                {posts?.map((item,index)=>{
                    return <div key={index} className="col-span-1">
       <div className="flex items-center gap-4">
-                            <img src="" className="w-[25px] h-[25px]"/>
-                            <h1>Masud Rana</h1>
+                            <img src={item.photoURL} className="w-[60px] h-[60px] rounded-full object-cover"/>
+                            <div className="w-full">
+                            <h1 className="font-bold text-xl ">{item.title}</h1>
+                            <div className="flex gap-2 items-center justify-between w-full">
+                            <h1 className="font-bold"><span className="text-sm font-medium">by</span> {item.displayName} ({item.role})</h1>
+                            <h1>{item.date}</h1>
                             </div>
-                            <h1 className="font-semibold">5 Tips to Kickstart Your Fitness Journey</h1>
-                            <p>Discover essential tips and strategies to begin your fitness journey and stay motivated. These practical steps will help you set achievable goals and maintain a healthy routine.</p>
-                            <Link><button>Read More</button></Link>
+                            </div>
+                            </div>
+                            <p className="w-full mt-3">{item.texts.slice(0,200)}...</p>
+                            <Link to={`post/${item._id}`}><button className="btn mt-3 bg-bgCommon text-white">Read More</button></Link>
                    </div>
                 })}
             </div>
