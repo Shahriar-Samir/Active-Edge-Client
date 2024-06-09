fc3c7aa41ed855bb5ea2f16a646c87c9d95d3a60
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-10 00:03:37 +0600

    designed newsletter section of home page

--- src/Pages/Home/Newsletter.jsx
+++ src/Pages/Home/Newsletter.jsx
@@ -21,16 +21,15 @@ const Newsletter = () => {
   }
 
     return (
-       <div>
-        <Heading title={'News Letter'} details={'This is newsletter section'}/>
-         <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+       <div className='mt-28 mx-auto w-11/12 max-w-[1200px]'>
+        <div className="flex justify-between items-center flex-col lg:flex-row gap-10">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Subscribe now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 className="text-5xl font-bold">Stay Updated with Our Newsletter</h1>
+            <p className="py-6 text-xl ">Join our newsletter to receive the latest updates on fitness tips, workout routines, nutrition advice, and exclusive offers. Stay connected with our community and never miss out on any exciting news or events. Subscribe now and take a step towards a healthier, more active lifestyle!</p>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form className="card-body" onSubmit={subscribe}>
+              <h1 className='text-2xl text-center font-bold'>Subscribe Newsletter</h1>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
@@ -44,12 +43,11 @@ const Newsletter = () => {
                 <input type="Email" name='email' placeholder="email" className="input input-bordered" required />
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Subscribe</button>
+                <button className="btn bg-bgCommon text-white hover:bg-bgHover">Subscribe</button>
               </div>
             </form>
           </div>
         </div>
-      </div>
        </div>
     );
 };
