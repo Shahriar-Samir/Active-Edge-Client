0d98b5c656f00300582619f2d435e7bc0658d29f
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-10 00:32:08 +0600

    designed footer section

--- src/Pages/Home/Team.jsx
+++ src/Pages/Home/Team.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import Heading from '../../Components/Heading';
+import Loading from '../../Components/Loading';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
 
 const Team = () => {
+  const axiosPublic = useAxiosPublic()
+    const {data,isFetching} = useQuery({
+        queryKey:["alltrainers"],
+        initialData: [],
+        queryFn: ()=>
+            axiosPublic.get('/allTrainers')
+            .then(res=>{
+                return res.data
+            })
+    })
+
+    if(isFetching){
+        return <Loading/>
+    }
     return (
-        <div>
-            <Heading title={'Our team'} details={'Full of members'}/>
-            <div className='grid grid-cols-3'>
-                    {[1,2,3].map((item,index)=>{
-                            return <Trainer key={index} item={item}/>
+        <div className='mt-28 w-11/12 mx-auto max-w-[1200px]'>
+            <Heading title={'Our Team'} details={''}/>
+            <div className='grid grid-cols-3 gap-5'>
+                    {data?.map((item,index)=>{
+                      return <Trainer key={index} item={item}/>
                     })}
             </div>
         </div>
@@ -21,10 +38,10 @@ const Trainer = ({item}) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img src="" alt="Shoes" className="rounded-xl" />
+          <img src={item.photoURL} alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">{item.displayName}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions">
             <button className="btn btn-primary">Buy Now</button>
