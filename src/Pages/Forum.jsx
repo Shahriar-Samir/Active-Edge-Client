18539bde395adb250639c47e669d41afb08bf69f
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-11 18:39:27 +0600

    added a get route that gets a specific user's post vote status

--- src/Pages/Forum.jsx
+++ src/Pages/Forum.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {useQuery} from '@tanstack/react-query'
 import { formatDistanceToNow } from 'date-fns';
 import useAxiosPublic from '../Hooks/useAxiosPublic';
+import useAxiosSecure from '../Hooks/useAxiosSecure'
 import Loading from '../Components/Loading';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaArrowDown, FaArrowUp } from "react-icons/fa6";
+import { AuthContext } from '../Providers/AuthProvider';
+import {toast,ToastContainer} from 'react-toastify'
+
 
 
 const Forum = () => {
@@ -61,6 +67,7 @@ const Forum = () => {
     console.log(currentButtons)
         return (
             <div className='w-11/12 mx-auto max-w-[1200px]'>
+                           <ToastContainer/>
                 <p className='mt-5 text-3xl font-bold text-center'>Forum Posts</p>
     {posts.length < 1? <div className='h-[80vh] flex justify-center items-center'>
         <h1 className='text-center font-bold text-xl'>There are no post available</h1>
@@ -96,11 +103,40 @@ const Forum = () => {
 export default Forum;
 
 const Post = ({post})=>{
-    const {texts,title,date,displayName,photoURL,role} = post
+    const {texts,title,date,displayName,photoURL,role,_id,uid,email} = post
     const [timeAgo,setTimeAgo] = useState('Loading...')
+    const {user} = useContext(AuthContext)
+    const navigate = useNavigate()
+    const axiosSecure = useAxiosSecure()
+    const axiosPublic = useAxiosPublic()
+    const [vote,setVote] = useState()
+    const [upVotes,setUpVotes] = useState()
+    const [downVotes,setDownVotes] = useState()
+
 
     useEffect(()=>{
-       
+        axiosSecure.get(`/myVoteStatus`,{params:{uid,postId:_id}})
+        .then(res=>{
+                setVote(res.data)
+        })
+    },[])
+    
+    useEffect(()=>{
+        axiosPublic.get(`/getUpVotes/${_id}`)
+        .then(res=>{
+                setUpVotes(res.data)
+        })
+        axiosPublic.get(`/getDownVotes/${_id}`)
+        .then(res=>{
+                setDownVotes(res.data)
+        })
+        .catch(()=>{
+            toast.error('Something went wrong')
+        })
+    },[vote])
+
+
+    useEffect(()=>{
         if(date){
             setInterval(()=>{
                 setTimeAgo(formatDistanceToNow(new Date(date), { addSuffix: true }))
@@ -108,9 +144,51 @@ const Post = ({post})=>{
         }
     },[date])
 
+
+    const upVote = ()=>{
+        const postId = _id
+        if(vote === 'noVote' || vote === 'downVote'){
+                 axiosSecure.delete('/removeDownVote', {params:{uid,postId}})
+            axiosSecure.post('/addUpVote', {uid,displayName,email,postId,type:'upVote'})
+        .then(()=>{
+                setVote('upVote')
+        })
+        }
+        if(vote === 'upVote'){
+            axiosSecure.delete('/removeUpVote', {params:{uid,postId}})
+        .then(()=>{
+                setVote('noVote')
+        })
+        }
+    }
+
+
+
+    const downVote = ()=>{
+        const postId = _id
+        if(vote === 'noVote' || vote === 'upVote'){
+            axiosSecure.delete('/removeUpVote', {params:{uid,postId}})
+            axiosSecure.post('/addDownVote', {uid,displayName,email,postId,type:'downVote'})
+        .then(()=>{
+                setVote('downVote')
+        })
+        }
+        if(vote === 'downVote'){
+            axiosSecure.delete('/removeDownVote', {params:{uid,postId}})
+        .then(()=>{
+                setVote('noVote')
+        })
+        }
+    }
+
+
+
+
+    if(post && vote && upVotes && downVotes){
+
     return(
         <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
-            
+
         <div className="flex justify-between items-center mb-5 text-gray-500">
         <div className="flex justify-between items-center">
             <div className="flex items-center space-x-4">
@@ -124,7 +202,22 @@ const Post = ({post})=>{
             <span className="text-sm">{timeAgo}</span>
         </div>
         <h2 className="mb-2 text-lg font-bold "><p>{title}</p></h2>
-        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{texts}</p>
+        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{texts?.slice(0,150)}{texts.length > 150? '...' :''}</p>
+        <div className='flex justify-between '>
+            <div className='flex gap-5 items-center'>
+            <div className={`${vote === 'upVote'? 'bg-blue-500' : 'bg-bgCommon'} p-2 flex items-center gap-2 rounded-md cursor-pointer text-white font-bold`} onClick={()=> user? upVote() : navigate('/login')}>
+            <FaArrowUp className='text-xl ' />
+                <p>{upVotes?.allUpVotes}</p>
+            </div>
+            <div className={`${vote === 'downVote'? 'bg-red-500' : 'bg-bgCommon'} p-2 flex items-center gap-2 rounded-md cursor-pointer text-white font-bold`}  onClick={()=> user? downVote() : navigate('/login')}>
+            <FaArrowDown className='text-xl' />
+            <p>{downVotes?.allDownVotes}</p>
+            </div>
+            </div>
+            <Link to={`/forum/post/${_id}`}><button className='btn'>View Post</button></Link>
+        </div>
     </article>
-    )
+    )}
+
+    return   <div className="skeleton h-32 w-full"></div>
 }
